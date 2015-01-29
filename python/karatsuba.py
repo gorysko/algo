815fbbc9c8c731cd9ceb2d2e19e89d57bebbd1ac
Igor.Lushchyk <gorysko@gmail.com>
2015-01-29 14:33:56 +0200

    yghuj

--- python/karatsuba.py
+++ python/karatsuba.py
@@ -1,23 +1,25 @@
 def karatsuba(first, second):
-    a , b = split_number(first)
-    c, d = split_number(second)
-    n = get_number_bit(first)
-    ac = a * c
-    bd = b * d
-    tmp = ((a + b) * (c + d)) - ac -bd
-    result = (10**n) * ac + (10**(n/2))*tmp + bd
-    return result
+    """karatsuba"""
+    first_1, first_2 = split_number(first)
+    second_1, second_2 = split_number(second)
+    num = get_number_bit(first)
+    firsts = first_1 * second_1
+    seconds = first_2 * second_2
+    tmp = ((first_1 + first_2) * (second_1 + second_2)) - firsts - seconds
+    return (10**num) * firsts + (10**(num/2))*tmp + seconds
 
 
 
 def split_number(num):
+    """spliting numbers into two parts"""
     num_bit = get_number_bit(num)
     first = str(num)[:num_bit /2]
     second = str(num)[num_bit/2:]
     return int(first), int(second)
 
 def get_number_bit(num):
+    """gets number range"""
     return len(str(num))
 
 if __name__ == '__main__':
-    print karatsuba(49823261, 44269423) == 2205647016448403
+    print karatsuba(49823261, 44269423) == 49823261 * 44269423
