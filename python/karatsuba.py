55f84fc8711030ea998988cc440ade6a0ed152d3
Igor.Lushchyk <gorysko@gmail.com>
2015-01-30 16:56:09 +0200

    ghnj

--- python/karatsuba.py
+++ python/karatsuba.py
@@ -1,25 +1,30 @@
 def karatsuba(first, second):
-    """karatsuba"""
-    first_1, first_2 = split_number(first)
-    second_1, second_2 = split_number(second)
-    num = get_number_bit(first)
-    firsts = first_1 * second_1
-    seconds = first_2 * second_2
-    tmp = ((first_1 + first_2) * (second_1 + second_2)) - firsts - seconds
-    return (10**num) * firsts + (10**(num/2))*tmp + seconds
+    """karatsuba algo"""
+    num = max(get_range(first), get_range(second))
+    num_2 = num / 2
+    if get_range(first) < 2 or get_range(second) < 2:
+        return first * second
+    first_a, first_b = split_num(first)
+    second_c, second_d = split_num(second)
+    firsts = karatsuba(first_a, second_c)
+    seconds = karatsuba(first_b, second_d)
+    thirds = karatsuba((first_a + first_b), (second_c + second_d)) - \
+        first - seconds
+    return  ((10**num)*firsts)+((10**num_2)*thirds) + seconds
 
 
 
-def split_number(num):
-    """spliting numbers into two parts"""
-    num_bit = get_number_bit(num)
-    first = str(num)[:num_bit /2]
-    second = str(num)[num_bit/2:]
-    return int(first), int(second)
+def get_range(num):
+    """Gets number range"""
+    return len(str(num)) - 1
 
-def get_number_bit(num):
-    """gets number range"""
-    return len(str(num))
+def split_num(num):
+    """splits number into two parts"""
+    num_range = int(round(get_range(num) / 2.0))
+    num = str(num)
+    print num_range
+    return int(num[:num_range]), int(num[num_range:])
 
 if __name__ == '__main__':
-    print karatsuba(49823261, 44269423) == 49823261 * 44269423
+    print karatsuba(111, 111)
+    print 2205647016448403
