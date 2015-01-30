9264c166ff5d25279960a85adf4ec2607f3e37f5
gorysko <gorysko@gmail.com>
2015-01-30 21:55:07 +0200

    final

--- python/karatsuba.py
+++ python/karatsuba.py
@@ -1,19 +1,17 @@
-COUNTER = 0
-
 def karatsuba(first, second):
     """karatsuba algo"""
+    if first < 10 or second < 10:
+        return first * second
     num = max(get_range(first), get_range(second))
     num_2 = num / 2
-    if get_range(first) < 3 or get_range(second) < 3:
-        return first * second
     first_a, first_b = split_num(first)
     second_c, second_d = split_num(second)
     firsts = karatsuba(first_a, second_c)
     seconds = karatsuba(first_b, second_d)
-    thirds = karatsuba(first_a, second_d) + \
-        karatsuba(first_b, second_c)
-    return  ((10**num)*firsts)+((10**num_2)*thirds) + seconds
 
+    thirds = karatsuba((first_a + first_b), (second_d + second_c)) - \
+        firsts - seconds
+    return  ((10**(2*num_2))*firsts)+((10**num_2)*thirds) + seconds
 
 
 def get_range(num):
@@ -26,6 +24,8 @@ def split_num(num):
     num = str(num)
     return int(num[:num_range]), int(num[num_range:])
 
+
+
 if __name__ == '__main__':
     print karatsuba(49823261, 44269423)
-    print 2205647016448403
+    print karatsuba(49823261, 44269423) == 2205647016448403
