f058379d02ec916865a7c5b2f97e42a18fd44217
Igor.Lushchyk <gorysko@gmail.com>
2015-01-31 23:56:51 +0200

    ghbnjkm

--- go/karatsuba.go
+++ go/karatsuba.go
@@ -1,11 +1,15 @@
 package main
 
-func get_range(num int) (rank int) {
+import (
+	"strconv"
+)
 
+func get_range(num int) (rank int) {
+	return len(strconv.Itoa(num))
 }
 
 func split_num(num int) (first, second int) {
-
+	num_range := int(get_range(num) / 2.)
 }
 
 func karatsuba(first, second int) (result int) {
