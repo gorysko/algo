a02bfe5c074c32071073e1f33ca5e8b4e7830c3a
Igor.Lushchyk <gorysko@gmail.com>
2015-01-31 23:44:54 +0200

    added go template for karatsuba

---
+++ go/karatsuba.go
@@ -0,0 +1,17 @@
+package main
+
+func get_range(num int) (rank int) {
+
+}
+
+func split_num(num int) (first, second int) {
+
+}
+
+func karatsuba(first, second int) (result int) {
+
+}
+
+func main() {
+
+}
