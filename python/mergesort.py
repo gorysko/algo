049108c13a5b406a88bc5d5820b9948d8aea4382
gorysko <gorysko@gmail.com>
2015-02-15 22:58:18 +0200

    fixed error

--- python/mergesort.py
+++ python/mergesort.py
@@ -2,29 +2,32 @@
 
 __author__ = "gorysko@gmail.com"
 __description__ = "Merge and sort algo for stanford course on coursera"
-
+counter = 0
 
 def merge_sort(array):
     """Meerge and sort algo."""
+    
     if len(array) <= 1:
         return array
-    middle = len(array) / 2
+    middle = len(array) // 2
     left = array[middle:]
     right = array[:middle]
     left = merge_sort(left)
     right = merge_sort(right)
-    return merge(left, right)
+    return merge(right, left)
 
 def merge(left, right):
     """merges arrays"""
+    global counter
     result = []
     while len(left) > 0 and len(right) > 0:
         if left[0] <= right[0]:
             result.append(left[0])
             left = left[1:]
-        else:
+        elif left[0] > right[0]:            
             result.append(right[0])
             right = right[1:]
+            counter += len(left)
     while len(left) > 0:
         result.append(left[0])
         left = left[1:]
@@ -32,5 +35,4 @@ def merge(left, right):
         result.append(right[0])
         right = right[1:]
     return result
-
 
